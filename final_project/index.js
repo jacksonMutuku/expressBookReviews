fa5805714856977923eeca75af8a1e03d8300019
jackson mutuku <mutukumumina99@gmail.com>
2024-05-27 06:29:50 -0400

    finished project

--- final_project/index.js
+++ final_project/index.js
@@ -11,9 +11,14 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+    if (req.session && req.session.user){
+        next()
+    } else {
+        res.status(401).send('Unauthorized: No session available')
+    }
 //Write the authenication mechanism here
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
